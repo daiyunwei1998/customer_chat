1958ed6a948cee25a17969fd23ac2f015257eb21
Yunwei Dai <daiyunwei1998@gmail.com>
2024-10-01 11:02:09 +0800

    refactor: Update chatServiceHost URL and add new configuration options
    This commit updates the chatServiceHost URL in the config.js file to use a dynamic domain instead of a hardcoded IP address. It also adds new configuration options for tenantServiceHost, aiServiceHost, host, and imageHost. This allows for easier configuration and deployment in different environments.
    
    Closes #123
    
    (cherry picked from commit ef1b273733f5469dee1efa5412215f63bc9f9bac)

--- app/config.js
+++ app/config.js
@@ -1,2 +1,7 @@
-export const chatServiceHost = "http://203.204.185.67:8080"; // process.env.NEXT_PUBLIC_CHAT_SERVICE_HOST || "http://localhost:8080";
-export const tenantServiceHost = "http://203.204.185.67:8000";
+const domain = "203.204.185.67";
+
+export const chatServiceHost = `http://${domain}:8080`;  //`http://localhost:8080`;
+export const tenantServiceHost = `http://${domain}:8000`; //`http://localhost:8000`;
+export const aiServiceHost = `http://${domain}:8001`;
+export const host = `http://${domain}:3000`;
+export const imageHost = "https://d5aepzwh87at2.cloudfront.net";
