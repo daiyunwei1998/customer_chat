71169867937c5d7f5341f005ac627ed71438c632
Yunwei Dai <daiyunwei1998@gmail.com>
2024-10-01 11:02:28 +0800

    refactor: Update chat UI and WebSocket connection setup in Chat.js
    (cherry picked from commit fda1a893f98c95f684388428a0c11f22caefaa85)

--- app/page.js
+++ app/page.js
@@ -4,15 +4,10 @@ import Chat from "./components/Chat";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div style={{width:"500px"}}>
-          <Chat></Chat> 
-        </div>
-        
+    <div className={styles.page} style={{ height: '100vh', width: '100vw', display: 'flex', flexDirection: 'column' }}>
+      <main className={styles.main} style={{ flex: 1, width: '100%', overflow: 'hidden'  }}>
+        <Chat tenantId="tenant_1" userId="test" />
       </main>
-      <footer className={styles.footer}>
-      </footer>
     </div>
   );
 }
