b52ab988d4e3f22874044f92ef1319ca5aba9106
Yunwei Dai <daiyunwei1998@gmail.com>
2024-10-03 19:49:08 +0800

    feat: change domain name configuration

--- app/config.js
+++ app/config.js
@@ -1,7 +1,6 @@
-const domain = "203.204.185.67";
+const chatServiceHost = "https://flashresponse.net/chat";
+const tenantServiceHost = "https://flashresponse.net/tenant";
+const aiServiceHost = "https://flashresponse.net/ai";
+const host = "https://flashresponse.net";
 
-export const chatServiceHost = `http://${domain}:8080`;  //`http://localhost:8080`;
-export const tenantServiceHost = `http://${domain}:8000`; //`http://localhost:8000`;
-export const aiServiceHost = `http://${domain}:8001`;
-export const host = `http://${domain}:3000`;
 export const imageHost = "https://d5aepzwh87at2.cloudfront.net";
